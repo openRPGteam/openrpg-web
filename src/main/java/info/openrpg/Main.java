ceb314f305ea160b5e758a728048b76d2c6edf3b
Dark Casual <dark.poison.casual@gmail.com>
2017-05-01 22:56:33 +0300

    [BF] removed credentials from gh

--- src/main/java/info/openrpg/Main.java
+++ src/main/java/info/openrpg/Main.java
@@ -6,6 +6,10 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class Main {
@@ -14,10 +18,23 @@ public class Main {
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        Properties properties = new Properties();
+        InputStream input;
         try {
-            telegramBotsApi.registerBot(new Bot(Credentials.corovaner_bot));
+            input = Main.class.getClassLoader().getResourceAsStream("application.properties");
+            properties.load(input);
+            Credentials botCredentials = Credentials
+                    .builder()
+                    .botName(properties.getProperty("bot.name"))
+                    .token(properties.getProperty("bot.token"))
+                    .build();
+            telegramBotsApi.registerBot(new Bot(botCredentials));
         } catch (TelegramApiRequestException e) {
             logger.warning(e.getMessage());
+        } catch (FileNotFoundException e) {
+            logger.warning("No file with properties was found");
+        } catch (IOException e) {
+            logger.warning("Exception while loading properties");
         }
 
     }
--- src/main/java/info/openrpg/telegram/Credentials.java
+++ src/main/java/info/openrpg/telegram/Credentials.java
@@ -1,14 +1,13 @@
 package info.openrpg.telegram;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum Credentials {
-    OpenRPGBot("OpenRPGBot", "360419088:AAFuT9do0fG68jBEpW4ETCVdXYiN5f_I46k"),
-    corovaner_bot("corovaner_bot", "334320004:AAEKbNEWJgKBHWWvc6W_OuUtTqCxucG7Vsg");
-
+@Builder
+public class Credentials {
     private String botName;
     private String token;
 }
